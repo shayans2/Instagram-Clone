f1c11690dedd58cee50f6b92f27239ea9b30e3bf
Shayan Bemainan <4848907-shayanBemanian@users.noreply.gitlab.com>
2020-05-09 13:08:11 +0430

    Refactored

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const config = require("config");
 const cors = require("cors");
@@ -7,7 +8,17 @@ const auth = require("./routes/auth");
 const posts = require("./routes/posts");
 const comments = require("./routes/comments");
 
-require("./services/db");
+// Database Config
+mongoose
+  .connect("mongodb://localhost/instagram", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(console.log("Connected to database"))
+  .catch((err) => console.log(new Error(err)));
+
 let port = config.get("port");
 app.listen(port, console.log(`Listening on port ${port}...`));
 
